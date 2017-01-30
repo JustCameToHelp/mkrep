e4134efaacbe90421f4c296f08772eb08a4b2184
JustCameToHelp <libor.pospichal@email.cz>
2017-01-30 22:40:22 +0100

    Experimenting with encoding

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<html>
+<head>
+
 <?php
 
 include "php/functions.php";
 include "php/functions-xpath.php";
 include "php/functions-formatting.php";
 
+header('Content-type: text/html; charset=utf-8');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -14,13 +18,23 @@ if($keyquery == NULL)
 }
 
 $krepUri = KrepUrl($keyquery);
-printfln("URI: %s", $krepUri);
+//printfln("URI: %s", $krepUri);
 
 $xpath = loadUriAsXPath($krepUri);
 
 $title = query_first($xpath, "/html/head/title");
-printfln($title);
+printf("<title>%s</title>", $title);
+?>
+</head>
+<body>
+<?php
+$listDiscussions = query_first($xpath,
+    "/html/body/div[@id='obal']/div[@id='obsah']/div[@id='odsazeni']/div[@id='sloupecD']");
+printfln("List: %s", $listDiscussions);
 
 print "<hr />";
 
 ?>
+
+</body>
+</html>
